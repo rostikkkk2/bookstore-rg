dbbc441668f872285d23435cc50957de12a3cd7e
rostikkkk2 <rost4402255@gmail.com>
2019-01-21 01:32:43 +0200

    Add sorting books by asc and desc

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,8 +1,21 @@
 class CategoriesController < ApplicationController
+  attr_accessor :books_category, :books
+
   def index
     @books = Book.all
     @categories = Category.all
+    @books_category = select_books_by_category
+    sort_books
+  end
+
+  def sort_books
+    # do sort with session???
+    @books_category = @books_category.order(:price) if params[:sort_price]
+    @books_category = @books_category.order(price: :desc) if params[:sort_price_desk]
+  end
+
+  def select_books_by_category
     books_chosen_category = @books.where(category_id: params[:category_id].to_i)
-    @books_category = params[:category_id] == 'all_categories' ? Book.all : books_chosen_category
+    return (params[:category_id] == 'all_categories') ? Book.all : books_chosen_category
   end
 end
